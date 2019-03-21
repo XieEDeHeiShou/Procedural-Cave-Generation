8e882543f687bc232889537cbac24d0342bbce8a
XieEDeHeiShou <2325690622@qq.com>
2019-03-21 22:41:23 +0800

    Extract inner class Triangle to outside

--- Unity Project/Assets/Scripts/Mesh/Generator/MeshGenerator.cs
+++ Unity Project/Assets/Scripts/Mesh/Generator/MeshGenerator.cs
@@ -294,30 +294,5 @@ namespace Mesh.Generator {
 
             return sharedTriangleCount == 1;
         }
-
-        private struct Triangle {
-            public readonly int vertexIndexA;
-            public readonly int vertexIndexB;
-            public readonly int vertexIndexC;
-            private readonly int[] _vertices;
-
-            public Triangle(int a, int b, int c) {
-                vertexIndexA = a;
-                vertexIndexB = b;
-                vertexIndexC = c;
-
-                _vertices = new int[3];
-                _vertices[0] = a;
-                _vertices[1] = b;
-                _vertices[2] = c;
-            }
-
-            public int this[int i] => _vertices[i];
-
-
-            public bool Contains(int vertexIndex) {
-                return vertexIndex == vertexIndexA || vertexIndex == vertexIndexB || vertexIndex == vertexIndexC;
-            }
-        }
     }
 }
